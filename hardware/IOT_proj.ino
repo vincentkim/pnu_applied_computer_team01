635c3604a1b545531e2f9c2930a882287282f365
wh77r77 <40201667+wh77r77@users.noreply.github.com>
2020-05-04 09:40:11 +0900

    Add files via upload

---
+++ hardware/IOT_proj.ino
@@ -0,0 +1,24 @@
+#include "lcd.h"
+#include "Temperature.h"
+
+int relay=52;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(relay,OUTPUT);
+  Lcd_init();
+  Temp_init();
+}
+void loop(){
+  int Set= Set_measure();
+  if(Set<20){
+    digitalWrite(relay,HIGH);
+  }
+  else{
+    digitalWrite(relay,LOW);
+  }
+  int Temp = Temp_measure();
+  Lcd_display(Set,Temp);
+}
+
+
