042d4e6b8120d60f785d7ed818bc22d8000e8115
wh77r77 <40201667+wh77r77@users.noreply.github.com>
2020-05-06 17:12:34 +0900

    Update Temperature.cpp
    기존 온도센서를 습도까지 측정 가능한 DHT11센서로 변경하고 이를 구현

--- hardware/libraries/Temperature/Temperature.cpp
+++ hardware/libraries/Temperature/Temperature.cpp
@@ -1,18 +1,32 @@
 #include "Arduino.h"
+#include <DHT11.h>
 #include "Temperature.h"
 
+DHT11 dht11(A1);
 
 void Temp_init(){
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
 }
+
 int Set_measure(){
   int Set = analogRead(A0);
   return map(Set,0,1023,0,30);
 }
+
 int Temp_measure(){
-  int fire = analogRead(A1);
-  float voltage = fire*5.0/1024.0;
-  return voltage*100;
+  int i;
+  float temp,humi;
+  if((i = dht11.read(humi,temp))==0){
+    return temp;
+  }
+}
+
+int humi_measure(){
+  int i;
+  float temp,humi;
+  if((i = dht11.read(humi,temp))==0){
+    return humi;
+  }
 }
 
