0067c8a9ab6572afd0e811721e6c7eaa2bd5900e
wh77r77 <40201667+wh77r77@users.noreply.github.com>
2020-05-06 20:58:20 +0900

    DHT11에 맞추어 함수들을 재정의

--- hardware/libraries/Temperature/Temperature.h
+++ hardware/libraries/Temperature/Temperature.h
@@ -1,14 +1,19 @@
 #ifndef temp_h
 #define temp_h
 
-void Temp_init();
 //가변 저항의 init.
-int Temp_measure();
-//DHT11 센서로 온도값을 받아온다.
-int humi_measere();
-//DHT11 센서로 습도값을 받아온다.
-int Set_measure();
-//기기 자체의 세팅 온도를 설정한다.
+void Temp_init();
 
+// String으로부터 온도값을 읽어온다.
+int Temp_measure(String str);
+
+// String으로부터 습도값을 읽어온다. 
+int Humi_measere(String str);
+
+//DHT11 센서로부터 온도값, 습도값을 String형태로 받아온다.
+String Set_measure();
+
+//기기 자체의 세팅 온도를 설정한다.
+String Sensor_measure();
 
 #endif
