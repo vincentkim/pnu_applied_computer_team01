6c3eeaebfda3b19e76ef80febd2386594be6e1ce
wh77r77 <40201667+wh77r77@users.noreply.github.com>
2020-07-01 16:07:34 +0900

    Update IOT_proj.ino

--- hardware/IOT_proj/IOT_proj.ino
+++ hardware/IOT_proj/IOT_proj.ino
@@ -1,33 +1,57 @@
 #include "lcd.h"
 #include "Temperature.h"
+#include "MyWifi.h"
+#include "WiFiEsp.h"
 
+
+
+
+//WiFiEspClient client;
+
+unsigned long lastConnectionTime = 0;
+
+// 30초마다 HTTP 요청
+const unsigned long postingInterval = 30000L;
+int Set;
 int relay=52;
-int Humi, Temp;
+int Humi,Temp;
+int pre_Humi,pre_Temp;
 String str;
-
-//초기 init
+String Mac_add;
 void setup(){
   Serial.begin(9600);
   pinMode(relay,OUTPUT);
+  Wifi_init();
   Lcd_init();
   Temp_init();
+  str = Sensor_measure();
+  pre_Temp = Temp_measure(str);
+  pre_Humi = Humi_measure(str);
+  Set= 21;
+  Lcd_display(Set,pre_Temp,pre_Humi);
+  //Set= Set_measure();
 }
-
 void loop(){
-  //현재 멀티탭 제어를 확인을 위하여 값의 변경이 쉬운 가변저항을 이용하여 릴레이 모듈 확인
-  //추후에 바꿀 예정
-  int Set= Set_measure();
-  //가변저항 값이 20미만이면 릴레이모듈이 작동하여 멀티탭의 전원이 켜짐
-  if(Set<20){
+
+  //현재 상태를 나타내기 위해 센서로부터 값을 읽어온다.
+  str = Sensor_measure();
+  Temp = Temp_measure(str);
+  Humi = Humi_measure(str);
+  Set=check_data();
+  if(millis()-lastConnectionTime>postingInterval){
+  http_req(Temp,Humi);
+  //Set=27;
+  lastConnectionTime=millis();
+  }
+  // 수신받은 설정 온도보다 낮으면 릴레이 모듈 작동하여 멀티탭 전원 켜짐
+  if(Temp<Set){
     digitalWrite(relay,HIGH);
   }
-  //가변저항 값이 20이상이면 릴레이모듈이 꺼지며 멀티탭의 전원이 꺼짐
+  //수신받은 설정 온도에 도달하면 릴레이 모듈이 꺼지며 멀티탭 전원 꺼짐
   else{
     digitalWrite(relay,LOW);
   }
   
-  str = Sensor_measure();
-  Temp = Temp_measure(str); 
-  Humi = humi_measure(str);
-  Lcd_display(Set, Temp, Humi);
+  Lcd_display(Set,Temp,Humi);
+  
 }
